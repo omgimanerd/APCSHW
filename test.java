382769ee76a92989e1fa119899e47e80562e4454
Alvin Lin <alvin.lin@stuypulse.com>
2014-09-19 14:04:26 -0400

    Removed test/demo java files

--- test.java
+++ test.java
@@ -1,10 +1,5 @@
 public class test {
     public static void main(String[] args) {
-	greeter bob = new greeter("hi dere");
-
-        bob.greeter();
-        bob.greeter();
-        bob.greeter();
-        bob.greeter();
+        System.out.println("Java works");
     }
 }
