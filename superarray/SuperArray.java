8f0827bb2fe19518af76119ec44c38dc897dff31
Alvin Lin <alvin.lin@stuypulse.com>
2014-11-06 13:39:14 -0500

    optimization

--- superarray/SuperArray.java
+++ superarray/SuperArray.java
@@ -2,30 +2,44 @@ public class SuperArray {
   
   private Object[] array_;
   
-  private int length_;
+  private int size_;
   
   public SuperArray() {
     this(10);
   }
   
   public SuperArray(int length) {
-    this.length_ = length;
     this.array_ = new Object[length];
+  }
+
+  public Object get(int index) {
+    if (index < 0 || index > this.array_.length) {
+      return null;
+    }
+    return this.array_[index];
+  }
+  
+  public void set(int index, Object object) {
+    if (this.array_[index] == null) {
+      this.size_++;
+    }
+    this.array_[index] = object;
+  }
+  
+  public void clear() {
+    for (int i = 0; i < this.array_.length; ++i) {
+      this.array_[i] = null;
+    }
+    this.size_ = 0;
   }
   
   public int size() {
-    int counter = 0;
-    for (int i = 0; i < this.length_; ++i) {
-      if (this.array_[i] != null) {
-        counter++;
-      }
-    }
-    return counter;
+    return this.size_;
   }
   
   public String toString() {
     String out = "[ ";
-    for (int i = 0; i < this.length_; ++i) {
+    for (int i = 0; i < this.array_.length; ++i) {
       out += this.array_[i] + " ";
     }
     out += "]";
@@ -33,18 +47,17 @@ public class SuperArray {
   }
   
   public void add(Object object) {
-    for (int i = 0; i < this.length_; ++i) {
+    for (int i = 0; i < this.array_.length; ++i) {
       if (this.array_[i] == null) {
         this.array_[i] = object;
         return;
       }
     }
-    this.resize(this.length_ + 1);
+    this.resize(this.array_.length + 1);
     this.add(object);
   }
   
   public void resize(int newCapacity) {
-    this.length_ = newCapacity;
     Object[] newArray = new Object[newCapacity];
     for (int i = 0; i < newCapacity; ++i) {
       if (i < this.array_.length) {
@@ -52,19 +65,12 @@ public class SuperArray {
       }
     }
     this.array_ = newArray;
-  }
-  
-  public void clear() {
-    for (int i = 0; i < this.length_; ++i) {
-      this.array_[i] = null;
+
+    this.size_ = 0;
+    for (int i = 0; i < newCapacity; ++i) {
+      if (this.array_[i] != null) {
+        this.size_++;
+      }
     }
-  }
-  
-  public Object get(int index) {
-    return this.array_[index];
-  }
-  
-  public void set(int index, Object object) {
-    this.array_[index] = object;
   }
 }
