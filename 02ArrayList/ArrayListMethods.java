3d990c08d69e59c566bc4129fc88246e7c71f336
Alvin Lin <alvin.lin@stuypulse.com>
2014-11-14 13:27:15 -0500

    docs?

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 public class ArrayListMethods {
+
+  /**
+   * Given an ArrayList L, deletes consecutive duplicate elements
+   * in L.
+   * @param L An arraylist to delete duplicate
+   *   elements in.
+   */
   public static void collapseDuplicates(ArrayList<Integer> L) {
     for (int i = 0; i < L.size() - 1; ++i) {
       if (L.get(i).equals(L.get(i + 1))) {
@@ -21,6 +28,11 @@ public class ArrayListMethods {
   */
 
   // 5 iterations of this runs for ~500 ms on my computer.
+  /**
+   * Given an ArrayList L, randomizes all the elements in L.
+   * @param L An ArrayList to randomize the elements
+   *   for.
+   */
   public static void randomize(ArrayList<Integer> L) {
     Random rand = new Random();
     for (int i = 0; i < L.size(); ++i) {
