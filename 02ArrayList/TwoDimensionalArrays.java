06f0fb5579566a133acaced100a9f50dc48ffc8e
Alvin Lin <alvin.lin@stuypulse.com>
2014-11-18 14:06:02 -0500

    noms

--- 02ArrayList/TwoDimensionalArrays.java
+++ 02ArrayList/TwoDimensionalArrays.java
@@ -1,6 +1,6 @@
 public class TwoDimensionalArrays {
   
-  public boolean contains(int value, int[] array) {
+  public static boolean contains(int value, int[] array) {
     for (int i = 0; i < array.length; ++i) {
       if (array[i] == value) {
         return true;
@@ -9,7 +9,7 @@ public class TwoDimensionalArrays {
     return false;
   }
 
-  public boolean contains(int value, int[][]array) {
+  public static boolean contains(int value, int[][]array) {
     for (int i = 0; i < array.length; ++i) {
       if (contains(value, array[i])) {
         return true;
@@ -20,5 +20,15 @@ public class TwoDimensionalArrays {
   
   public static void main(String args[]) {
     int[][] a = new int[3][3];
+    
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        a[i][j] = 0;
+        System.out.println(a[i][j]);
+      }
+    }
+    a[0][2] = 3;
+
+    System.out.println(contains(3, a));
   } 
 }
