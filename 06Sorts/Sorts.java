13923ddeea7a95687a8428a6bc049bd69b85739a
Alvin Lin <alvin.lin@stuypulse.com>
2014-12-16 13:24:47 -0500

    herp

---
+++ 06Sorts/Sorts.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Sorts {
+
+  public static void randomize(int[] c) {
+    Random rand = new Random();
+    int toSwap, tmp;
+    for (int i = 0; i < c.length; ++i) {
+      toSwap = rand.nextInt(c.length - i) + i;
+      if (toSwap == i) {
+        continue;
+      }
+      tmp = c[i];
+      c[i] = c[toSwap];
+      c[toSwap] = tmp;
+    }
+  }
+
+  public static void bubbleSort(int[] c) {
+    int tmp = 0;
+    for (int i = c.length - 1; i > 0; --i) {
+      for (int j = 0; j < i - 1; ++j) {
+        if (c[j] > c[j + 1]) {
+          tmp = c[j + 1];
+          c[j + 1] = c[j];
+          c[j] = tmp;
+        }
+      }
+    }
+  }
+
+  public static void insertionSort(int[] c) {
+    int tmp, track;
+    for (int i = 1; i < c.length; ++i) {
+      tmp = c[i];
+      track = i;
+      while (track > 0 && tmp < c[track - 1]) {
+        c[i] = c[track - 1];
+        track--;
+      }
+      c[track] = tmp;
+    }
+  }
+
+  public static void selectionSort(int[] c) {
+    int minIndex, tmp;
+    for (int i = 0; i < c.length; ++i) {
+      minIndex = i;
+      for (int j = i; j < c.length; ++j) {
+        if (c[i] < c[j]) {
+          minIndex = j;
+        }
+      }
+      tmp = c[i];
+      c[i] = c[minIndex];
+      c[minIndex] = tmp;
+    }
+  }
+
+  public static void out(int[] c) {
+    String out = "[ ";
+    for (int i = 0; i < c.length - 1; ++i) {
+      out += c[i] + ", ";
+    }
+    out += c[c.length - 1] + " ]";
+    System.out.println(out);
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[100];
+    for (int i = 0; i < arr.length; ++i) {
+      arr[i] = i;
+    }
+    randomize(arr);
+    out(arr);
+    bubbleSort(arr);
+    out(arr);
+  }
+}
